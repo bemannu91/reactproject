7a978cf76a5fe5a1480daeaab3cf76c1032b5511
bemannu91 <andriei.kudlo@gmail.com>
2026-04-23 01:17:09 +0500

    delet all

--- components/PageViewSet.jsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "./Header";
-import { Set } from "./Set";
-
-export function PageViewSet() {
-    return <>
-        <Header />
-        <Set />
-    </>;
-}
--- components/useInput.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-export function useInput(initialValue) {
-    const [value, setValue] = useState(initialValue);
-    const handleChange = (e) => setValue(e.target.value);
-    return [value, handleChange];
-}
